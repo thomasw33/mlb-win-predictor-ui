fc8649b95d9fb6d5dbf071918d7111978c3db99b
thomasw33 <144080706+thomasw33@users.noreply.github.com>
2025-04-29 17:09:40 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+# This is where I imported everything I needed for the project. I did it at the beginning so that it would not get confusing later
 import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
 
+# This is what loads the model that I brought over from the Colab Notebook and reads the CSV file that contains the testing data
 model = joblib.load("model.pkl")
 test_data = pd.read_csv("MLB_test.csv")
 
+# This is where I create 2 new features Payroll per win and log salary (as in logarithm)
 test_data["Payroll_per_Win"] = test_data["Total_Payroll"] / test_data["Wins"]
 test_data["Log_Salary"] = np.log(test_data["Total_Payroll"])
 
+# This is the initial setup for the Streamlit App
 st.title("MLB Win Predictor (2019–2024)")
 
 team_selected = st.selectbox("Select a Team:", sorted(test_data["Team"].unique()))
 year_selected = st.selectbox("Select a Year:", sorted(test_data["Year"].unique()))
 
+# This is where the app predicts the wins based off of the team and year that the user selects
 if st.button("Predict"):
     row = test_data[(test_data["Team"] == team_selected) & (test_data["Year"] == year_selected)]
 
@@ -32,7 +37,7 @@ if st.button("Predict"):
         }])
 
         predicted_wins = model.predict(X_input)[0]
-
-        st.markdown(f"**Payroll**: ${total_payroll:,.0f}")
-        st.success(f"**Predicted Wins**: {predicted_wins:.1f}")
-        st.info(f"**Actual Wins**: {actual_wins}")
+	# This is what is output when the predict button is selected
+        st.markdown(f"**Team Payroll**: ${total_payroll:,.0f}")
+        st.success(f"**Predicted Win Total**: {predicted_wins:.1f}")
+        st.info(f"**Actual Win Total**: {actual_wins}")
